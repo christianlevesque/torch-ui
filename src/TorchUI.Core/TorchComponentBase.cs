ead7e0afc06dbb91d8e2033a89260559cede249f
christianlevesque <christian@levesque.dev>
2025-04-20 18:56:30 -0500

    added base torch component with attribute splatting target

---
+++ src/TorchUI.Core/TorchComponentBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace TorchUI;
+
+/// <summary>
+/// A base component for all TorchUI components
+/// </summary>
+public class TorchComponentBase : ComponentBase
+{
+	/// <summary>
+	/// Captures additional HTML attributes supplied by the developer
+	/// </summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public Dictionary<string, object?> UserAttributes { get; set; } = new();
+}
