5ac431bff774970efd9201ea75b42960c35e2884
christianlevesque <christian@levesque.dev>
2025-04-21 14:27:21 -0500

    added CssBuilder class to handle adding building CSS class name lists

---
+++ src/TorchUI.Core/CssBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace TorchUI;
+
+/// <summary>
+/// Represents a group of CSS classes that can be appended to and built
+/// </summary>
+public class CssBuilder
+{
+	private readonly List<string> _classes = [];
+
+	/// <summary>
+	/// Adds a CSS class to the <c>CssBuilder</c>
+	/// </summary>
+	/// <param name="className">The CSS class to add</param>
+	/// <returns>the <c>CssBuilder</c></returns>
+	public CssBuilder AddClass(string? className)
+	{
+		if (!string.IsNullOrEmpty(className))
+		{
+			_classes.Add(className);
+		}
+
+		return this;
+	}
+
+	/// <summary>
+	/// Adds a CSS class to the <c>CssBuilder</c> if a condition is true
+	/// </summary>
+	/// <param name="className">The CSS class to add</param>
+	/// <param name="flag">The condition to check</param>
+	/// <returns>the <c>CssBuilder</c></returns>
+	public CssBuilder AddClass(string? className, bool flag)
+		=> flag ? AddClass(className) : this;
+
+	/// <summary>
+	/// Builds the final CSS string
+	/// </summary>
+	/// <returns>the CSS string, if any</returns>
+	public string? Build()
+		=> _classes.Count > 0 ? string.Join(' ', _classes) : null;
+}
