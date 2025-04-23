47a42d80a4ed0008263534898d5071c5cbc8cea7
Christian LeVesque <christian@levesque.dev>
2025-04-23 15:25:51 -0500

    added Bootstrap .btn-group component

---
+++ src/TorchUI.Bootstrap/Bootstrap/Components/ButtonGroup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents a Bootstrap <c>.btn-group</c>
+/// </summary>
+public class ButtonGroup : TorchComponentBase
+{
+	/// <summary>
+	/// The ARIA label to describe the button group
+	/// </summary>
+	/// <remarks>
+	/// This value will not override an existing <c>aria-label</c> attribute.
+	/// </remarks>
+	[Parameter]
+	public string? Label { get; set; }
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		CssBuilder.AddClass("btn-group");
+		GetOrSetAttribute("role", "group");
+
+		if (!string.IsNullOrEmpty(Label))
+		{
+			GetOrSetAttribute("aria-label", Label);
+		}
+
+		BuildHtml(builder);
+	}
+}
