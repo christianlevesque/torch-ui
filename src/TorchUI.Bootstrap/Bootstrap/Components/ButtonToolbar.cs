8ec0f197a4639687fa693dc805a9b50d09dd4359
Christian LeVesque <christian@levesque.dev>
2025-04-23 15:26:03 -0500

    added Bootstrap .btn-toolbar component

---
+++ src/TorchUI.Bootstrap/Bootstrap/Components/ButtonToolbar.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents a Bootstrap <c>.btn-toolbar</c>
+/// </summary>
+public class ButtonToolbar : TorchComponentBase
+{
+	/// <summary>
+	/// The ARIA label to describe the button toolbar
+	/// </summary>
+	/// <remarks>
+	/// This value will not override an existing <c>aria-label</c> attribute.
+	/// </remarks>
+	[Parameter]
+	public string? Label { get; set; }
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		CssBuilder.AddClass("btn-toolbar");
+		GetOrSetAttribute("role", "toolbar");
+
+		if (!string.IsNullOrEmpty(Label))
+		{
+			GetOrSetAttribute("aria-label", Label);
+		}
+
+		BuildHtml(builder);
+	}
+}
