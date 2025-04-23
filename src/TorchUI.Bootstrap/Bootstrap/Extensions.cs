5b0ed56d2308fe3e048b144367de076770a7bf31
christianlevesque <christian@levesque.dev>
2025-04-22 21:03:27 -0500

    added extension method to get the string value of a theme color

---
+++ src/TorchUI.Bootstrap/Bootstrap/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Contains extension methods used within the TorchUI.Bootstrap assembly
+/// </summary>
+public static class Extensions
+{
+	/// <summary>
+	/// Returns the lowercase string value of the given <see cref="ThemeColor"/>
+	/// </summary>
+	/// <param name="self">The theme color</param>
+	/// <returns>the theme color name as a lowercase string</returns>
+	public static string GetThemeColor(this ThemeColor self)
+		=> self.ToString().ToLowerInvariant();
+}
