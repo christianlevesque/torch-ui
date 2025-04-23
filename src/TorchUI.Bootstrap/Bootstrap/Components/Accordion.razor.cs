65ebf3fd475384bdb3a5efa5d51c70a6424dd254
christianlevesque <christian@levesque.dev>
2025-04-22 20:17:20 -0500

    added the Bootstrap accordion component with additional support for flush accordions and always-open accordions

---
+++ src/TorchUI.Bootstrap/Bootstrap/Components/Accordion.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents a Bootstrap <c>.accordion</c>
+/// </summary>
+public partial class Accordion
+{
+	private string? _id;
+
+	/// <summary>
+	/// Whether to remove some borders and rounded corners. Corresponds to the Bootstrap <c>.accordion-flush</c> class
+	/// </summary>
+	[Parameter]
+	public bool Flush { get; set; }
+
+	/// <summary>
+	/// Whether child accordion items should stay open even when another accordion item opens
+	/// </summary>
+	[Parameter]
+	public bool AlwaysOpen { get; set; }
+
+	/// <inheritdoc/>
+	protected override void OnInitialized()
+	{
+		if (!AlwaysOpen)
+		{
+			_id = GetAttributeWithDefault(
+				"id",
+				Guid.NewGuid().ToString());
+		}
+
+		CssBuilder.AddClass("accordion");
+		CssBuilder.AddClass("accordion-flush", Flush);
+
+		base.OnInitialized();
+	}
+}
