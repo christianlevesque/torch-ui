ddcf22b9fa10f1cc93b672b6ce0bad46243e6e82
Christian LeVesque <christian@levesque.dev>
2025-04-23 15:02:29 -0500

    added Bootstrap .breadcrumb-item component

---
+++ src/TorchUI.Bootstrap/Bootstrap/Components/BreadcrumbItem.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents a Bootstrap <c>.breadcrumb-item</c>
+/// </summary>
+public partial class BreadcrumbItem
+{
+	/// <summary>
+	/// Whether the current item is active
+	/// </summary>
+	/// <remarks>
+	/// If this value is <see langword="true"/>, the breadcrumb item is rendered without a link to the provided <see cref="Href"/>.
+	/// </remarks>
+	[Parameter]
+	public bool Active { get; set; }
+
+	/// <summary>
+	/// The target of the breadcrumb link
+	/// </summary>
+	/// <remarks>
+	/// If the <see cref="Active"/> parameter is <see langword="true"/>, this parameter is unused because the breadcrumb item is rendered without a link.
+	/// </remarks>
+	[Parameter]
+	public string? Href { get; set; }
+
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		CssBuilder.AddClass("breadcrumb-item");
+		CssBuilder.AddClass("active", Active);
+		base.OnInitialized();
+	}
+}
