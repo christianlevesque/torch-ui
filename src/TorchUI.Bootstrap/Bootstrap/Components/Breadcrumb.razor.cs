42ce4c5e8ffd02e0fb0587c10c6496278f4bab1c
Christian LeVesque <christian@levesque.dev>
2025-04-23 15:02:18 -0500

    added Bootstrap .breadcrumb component

---
+++ src/TorchUI.Bootstrap/Bootstrap/Components/Breadcrumb.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents a Bootstrap <c>.breadcrumb</c>
+/// </summary>
+public partial class Breadcrumb
+{
+	/// <summary>
+	/// The ARIA label of the breadcrumb list to provide for screen readers
+	/// </summary>
+	[Parameter]
+	public string Label { get; set; } = "breadcrumb";
+}
