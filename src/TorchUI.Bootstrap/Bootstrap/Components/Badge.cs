ab2ee6cec0917762a75c9cbc8cbf3cfdfa974105
Christian LeVesque <christian@levesque.dev>
2025-04-23 09:55:10 -0500

    added bootstrap .badge component

---
+++ src/TorchUI.Bootstrap/Bootstrap/Components/Badge.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents a Bootstrap <c>.badge</c>
+/// </summary>
+public class Badge : TorchComponentBase
+{
+	/// <summary>
+	/// The Bootstrap theme color of the badge
+	/// </summary>
+	[Parameter]
+	public ThemeColor Color { get; set; }
+
+	/// <summary>
+	/// Whether the badge should be a pill 
+	/// </summary>
+	[Parameter]
+	public bool Pill { get; set; }
+
+	public Badge()
+	{
+		Tag = "span";
+	}
+
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		CssBuilder.AddClass("badge");
+
+		base.OnInitialized();
+	}
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		CssBuilder.AddClass($"text-bg-{Color.GetThemeColor()}");
+
+		if (Pill)
+		{
+			CssBuilder.AddClass("rounded-pill");
+		}
+
+		BuildHtml(builder);
+	}
+}
