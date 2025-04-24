f4b627dd3a8e986b0d015a6a4808d506c5a66950
christianlevesque <christian@levesque.dev>
2025-04-24 17:38:40 -0500

    added Bootstrap .card-img-overlay component

---
+++ src/TorchUI.Bootstrap/Components/Cards/CardImageOverlay.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.card-img-overlay</c>
+/// </summary>
+public class CardImageOverlay : TorchComponentBase
+{
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		CssBuilder.AddClass("card-img-overlay");
+
+		BuildHtml(builder);
+	}
+}
