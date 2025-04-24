499b3eb17824ee80c8fb56ba65c3e78ad229c097
christianlevesque <christian@levesque.dev>
2025-04-24 17:07:42 -0500

    added Bootstrap .card-group component

---
+++ src/TorchUI.Bootstrap/Components/Cards/CardGroup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.card-group</c>
+/// </summary>
+public class CardGroup : TorchComponentBase
+{
+	/// <inheritdoc/>
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		CssBuilder.AddClass("card-group");
+
+		BuildHtml(builder);
+	}
+}
