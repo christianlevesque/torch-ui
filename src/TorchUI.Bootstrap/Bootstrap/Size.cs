2f2ab907a6184682ab99864135c0662f578fd7fd
christianlevesque <christian@levesque.dev>
2025-04-24 13:10:49 -0500

    added Bootstrap size enum

---
+++ src/TorchUI.Bootstrap/Bootstrap/Size.cs
@@ -0,0 +1,22 @@
+﻿namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents the available sizes in Bootstrap
+/// </summary>
+public enum Size
+{
+	/// <summary>
+	/// Represents the small size in Bootstrap
+	/// </summary>
+	Small,
+
+	/// <summary>
+	/// Represents the default size in Bootstrap
+	/// </summary>
+	Medium,
+
+	/// <summary>
+	/// Represents the large size in Bootstrap
+	/// </summary>
+	Large
+}
