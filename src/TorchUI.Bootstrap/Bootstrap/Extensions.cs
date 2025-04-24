f0673363627ba94f81d0319f553242a3c38ec948
christianlevesque <christian@levesque.dev>
2025-04-24 13:42:49 -0500

    added extension method to get an enum's [Description] if it is defined

--- src/TorchUI.Bootstrap/Bootstrap/Extensions.cs
+++ src/TorchUI.Bootstrap/Bootstrap/Extensions.cs
@@ -1,10 +1,29 @@
-﻿namespace TorchUI.Bootstrap;
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace TorchUI.Bootstrap;
 
 /// <summary>
 /// Contains extension methods used within the TorchUI.Bootstrap assembly
 /// </summary>
 public static class Extensions
 {
+	/// <summary>
+	/// Gets the value of the <see cref="DescriptionAttribute"/> if defined, or else the name of the enum member as a string
+	/// </summary>
+	/// <param name="self">The enum member</param>
+	/// <returns>the description</returns>
+	public static string GetDescription(this Enum self)
+	{
+		var stringified = self.ToString();
+		var description = self
+			.GetType()
+			.GetField(stringified)
+			?.GetCustomAttribute<DescriptionAttribute>();
+		return description?.Description ?? stringified;
+	}
+
 	/// <summary>
 	/// Returns the lowercase string value of the given <see cref="ThemeColor"/>
 	/// </summary>
