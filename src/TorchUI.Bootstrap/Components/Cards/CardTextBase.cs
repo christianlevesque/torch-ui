3157faaba2c884e66ff0e281e55f9b68dea15e4f
christianlevesque <christian@levesque.dev>
2025-04-24 17:02:00 -0500

    added a CardTextBase component which contains common rendering logic for text-based card content

---
+++ src/TorchUI.Bootstrap/Components/Cards/CardTextBase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using TorchUI.Bootstrap.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// A base card component which provides common rendering logic for the text-based parts of Bootstrap cards
+/// </summary>
+public abstract class CardTextBase : TorchComponentBase
+{
+	private readonly string _baseCssClass;
+
+	/// <summary>
+	/// The bootstrap color of the card text
+	/// </summary>
+	[Parameter]
+	public ThemeColor? TextColor { get; set; }
+
+	protected CardTextBase(string baseCssClass)
+	{
+		_baseCssClass = baseCssClass;
+	}
+
+	/// <inheritdoc/>
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		CssBuilder.AddClass(_baseCssClass);
+
+		if (TextColor is not null)
+		{
+			CssBuilder.AddClass(TextColor.Value.GetTextColorClass());
+		}
+
+		BuildHtml(builder);
+	}
+}
