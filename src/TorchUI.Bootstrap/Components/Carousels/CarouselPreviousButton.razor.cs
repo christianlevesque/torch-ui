b47a4c4b8bc3ce119307dd87b60ba069d7baae79
christianlevesque <christian@levesque.dev>
2025-04-30 13:54:54 -0500

    added Bootstrap .carousel-control-prev component

---
+++ src/TorchUI.Bootstrap/Components/Carousels/CarouselPreviousButton.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.carousel-control-previous</c>
+/// </summary>
+public partial class CarouselPreviousButton
+{
+	[CascadingParameter(Name = "CarouselId")]
+	private string ParentId { get; set; } = string.Empty;
+
+	protected override void OnInitialized()
+	{
+		CssBuilder.AddClass("carousel-control-prev");
+		base.OnInitialized();
+	}
+}
+
