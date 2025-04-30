c631a49d9ba8b3d50cfe688501da64bb8bbe0ca5
christianlevesque <christian@levesque.dev>
2025-04-30 13:55:25 -0500

    added Bootstrap .carousel-control-next component

---
+++ src/TorchUI.Bootstrap/Components/Carousels/CarouselNextButton.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.carousel-control-next</c>
+/// </summary>
+public partial class CarouselNextButton
+{
+	[CascadingParameter(Name = "CarouselId")]
+	private string ParentId { get; set; } = string.Empty;
+
+	protected override void OnInitialized()
+	{
+		CssBuilder.AddClass("carousel-control-next");
+		base.OnInitialized();
+	}
+}
+
