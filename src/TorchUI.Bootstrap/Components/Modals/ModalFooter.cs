b0f6499f4500622e84adf8501039f3a4dad105c2
christianlevesque <christian@levesque.dev>
2025-05-03 15:06:50 -0500

    added Bootstrap .modal-footer component

---
+++ src/TorchUI.Bootstrap/Components/Modals/ModalFooter.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.modal-footer</c>
+/// </summary>
+public class ModalFooter : TorchComponentBase
+{
+	/// <inheritdoc/>
+	protected override void SetupAttributes()
+	{
+		CssBuilder.AddClass("modal-footer");
+	}
+}
