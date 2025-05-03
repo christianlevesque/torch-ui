4b1e473449cbb39beefaf4cb8ec4216f9bba82bf
christianlevesque <christian@levesque.dev>
2025-05-03 16:01:34 -0500

    added a fourth size, ExtraLarge

--- src/TorchUI.Bootstrap/Size.cs
+++ src/TorchUI.Bootstrap/Size.cs
@@ -22,5 +22,14 @@ public enum Size
 	/// Represents the large size in Bootstrap
 	/// </summary>
 	[Description("lg")]
-	Large
+	Large,
+
+	/// <summary>
+	/// Represents the extra-large size in Bootstrap
+	/// </summary>
+	/// <remarks>
+	/// NOTE: Most Bootstrap components don't support the XL size. If a component parameter accepts a <see cref="Size"/> value, only use the XL size if the parameter's XMLDoc explicitly states that it supports the XL size.
+	/// </remarks>
+	[Description("xl")]
+	ExtraLarge
 }
